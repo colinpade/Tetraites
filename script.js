ba6f99a392923f089c22c01ba72f3861796ba287
Colin Pade <colin@saucelabs.com>
2015-06-21 12:42:04 -0700

    click thumbnails loads bigger image

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ angular.module('PictureApp', [])
   forecast.success(function(data) {
     $scope.album = data;
     $scope.aName = data.album.name;
+    $scope.mainImageURL = data.photos[0].url;
+
+    $scope.changeMainImage = function(imageURL) {
+      $scope.mainImageURL = imageURL;
+    };
   });
 }])
 .factory('forecast', ['$http', function($http) {
